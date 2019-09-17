e6e81064fabc85d90967fa8d9ea1668e8745bddd
Jagoda <jagoda.kosinska87@gmail.com>
2019-09-17 14:20:51 +0200

    initial commit&and complited editor

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,30 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
 
 
-console.log(`Hello world!`);
+const message = document.querySelector('.form__message--js');
+const load = document.querySelector('.form__button--load--js');
+const save = document.querySelector('.form__button--save--js');
 
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('message', message.value);
+});
 
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    message.value = localStorage.getItem('message');
+});
